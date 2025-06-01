26405a37d0181c85a0f36abcd3e4f2b1094c86e0
.asm <irajmirzazadeh2024@gmail.com>
2025-06-01 21:18:02 +0330

    Create code24.rs

---
+++ code24.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let a = 10;
+    let b = 3;
+
+    println!("{}", a + b);
+    println!("{}", a - b);
+    println!("{}", a * b);
+    println!("{}", a / b);
+    println!("{}", a % b);
+}
