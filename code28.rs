6f6995da8a199b2ed52ef841105ec2802be6ed7a
.asm <irajmirzazadeh2024@gmail.com>
2025-06-01 21:57:41 +0330

    Create code28.rs

---
+++ code28.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut x = 10;
+    x += 2;
+    println!("{}", x);
+    x -= 1;
+    println!("{}", x);
+    x *= 3;
+    println!("{}", x);
+    x /= 2;
+    println!("{}", x);
+    x %= 4;
+    println!("{}", x);
+}
