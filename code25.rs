eb2dc59c47cb34aa2353dd807c3b533b87484171
.asm <irajmirzazadeh2024@gmail.com>
2025-06-01 21:36:18 +0330

    Create code25.rs

---
+++ code25.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 5;
+    let b = 8;
+
+    println!("{}", a == b);
+    println!("{}", a != b);
+    println!("{}", a > b);
+    println!("{}", a < b);
+    println!("{}", a >= b);
+    println!("{}", a <= b);
+}
