b58e4152eb3d36c96d11a68acf98c767ed0b0436
.asm <irajmirzazadeh2024@gmail.com>
2025-06-08 21:07:02 +0330

    Create code32.rs

---
+++ code32.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let x = 0;
+    if x > 0 { //true
+        println!("greater");
+    }
+    else if x==0{
+        println!("equal");
+    }
+    else if x<0{
+        println!("smaller");
+    }
+    else{
+        println!(":)")
+    }
+}
